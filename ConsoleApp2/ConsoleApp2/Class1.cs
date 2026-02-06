b14429050ca550c1df3242a7447f5c598edca7fa
Kurai Yami <karandash082008@gmail.com>
2026-02-06 14:13:43 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Account
+    {
+        public string owner;
+        public string number;
+        public double percent;
+        public double balance;
+
+        public void ChangeOwner(string newOwner)
+        {
+            owner = newOwner;
+            Console.WriteLine($"Новый владелец: {owner}");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Снято: {amount} руб.");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Недостаточно средств!");
+            }
+        }
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Внесено: {amount} руб.");
+        }
+
+        public void AddPercent()
+        {
+            double interest = balance * percent / 100;
+            balance += interest;
+            Console.WriteLine($"Начислено процентов: {interest:F2} руб.");
+        }
+
+        public void ToDollars(double rate = 75.0)
+        {
+            double dollars = balance / rate;
+            Console.WriteLine($"{balance} руб. = {dollars:F2} $");
+        }
+
+        public void ToEuros(double rate = 85.0)
+        {
+            double euros = balance / rate;
+            Console.WriteLine($"{balance} руб. = {euros:F2} Е");
+        }
+
+        public void ShowInWords()
+        {
+            int rubles = (int)balance;
+            int kopecks = (int)((balance - rubles) * 100);
+            Console.WriteLine($"{rubles} рублей {kopecks:00} копеек");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp2;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== ЗАДАНИЕ 2.12: БАНКОВСКИЙ СЧЕТ ===\n");
+
+        Account myAccount = new Account
+        {
+            owner = "Иванов Иван Иванович",
+            number = "1234567890",
+            percent = 5.5,
+            balance = 10000.50
+        };
+
+        Console.WriteLine("ТЕКУЩИЙ СЧЕТ:");
+        Console.WriteLine($"Владелец: {myAccount.owner}");
+        Console.WriteLine($"Номер: {myAccount.number}");
+        Console.WriteLine($"Процент: {myAccount.percent}%");
+        Console.WriteLine($"Баланс: {myAccount.balance:F2} руб.");
+
+        Console.WriteLine("\nВЫПОЛНЯЕМ ОПЕРАЦИИ");
+
+        myAccount.Deposit(5000);
+        myAccount.Withdraw(2000);
+        myAccount.ChangeOwner("Петров Петр Петрович");
+        myAccount.AddPercent();
+
+        Console.WriteLine("\nКОНВЕРТАЦИЯ В ВАЛЮТУ");
+        myAccount.ToDollars(80);
+        myAccount.ToEuros(90);
+
+        Console.WriteLine("\nИТОГОВОЕ СОСТОЯНИЕ");
+        Console.WriteLine($"Баланс: {myAccount.balance:F2} руб.");
+        myAccount.ShowInWords();
+
+        Console.WriteLine("\nНажмите Enter для выхода...");
+        Console.ReadLine();
+    }
+}
